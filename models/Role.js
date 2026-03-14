e5f7d99569a241c1ab1185fd19d34a57b2742deb
DoanMinhPhi <phidoan1882004@gmail.com>
2026-03-14 12:53:43 +0700

    code bt

---
+++ models/Role.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const RoleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    description: {
+        type: String,
+        default: ""
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Role", RoleSchema);
